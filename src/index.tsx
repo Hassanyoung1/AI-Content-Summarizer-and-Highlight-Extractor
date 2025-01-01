37e68fec824261d1bfa7e8ccb6909b8bccc06f01
root <root@USER.localdomain>
2025-01-01 14:48:19 -0800

    file trees setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/tailwind.css";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Failed to find the root element.");
+}
+
